71293d800de28feac02917f0df3c4d8ec7bc22c1
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-23 23:32:12 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package days_15;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String[] namabulan = {"mei", "juni", "juli", "agustus"};
+for (int i = 0; i < namabulan.length; i++) {
+System.out.println(namabulan[i]);
+}
+}
+        }
+
+    
+    
